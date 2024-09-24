9775baaa889bb82a5c9ca8102c5c8f3261037d5f
haitaa <mustafahaitaa@gmail.com>
2024-09-24 18:23:25 +0300

    Responsive Menu Design

---
+++ src/app/(dashboard)/teacher/page.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const TeacherPage = () => {
+  return <div className="">Teacher Page</div>;
+};
+
+export default TeacherPage;
---
+++ src/app/sign-in/page.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const LoginPage = () => {
+  return <div className="">Login Page</div>;
+};
+
+export default LoginPage;
